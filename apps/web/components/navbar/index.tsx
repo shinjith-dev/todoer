63012bff3f4eb97db06220060237a271ae7834e3
shinjith-dev <shinjithpallikkara@gmail.com>
2024-08-22 19:58:00 +0530

    feat(web): add add-task button
    Signed-off-by: shinjith-dev <shinjithpallikkara@gmail.com>

--- apps/web/components/navbar/index.tsx
+++ apps/web/components/navbar/index.tsx
@@ -1,3 +1,6 @@
+import { IconScriptPlus } from "@tabler/icons-react";
+import { Button } from "../ui/button";
+
 export default async function Navbar() {
   return (
     <div className="absolute left-0 top-0 z-[999] w-screen">
@@ -5,6 +8,11 @@ export default async function Navbar() {
         <h3 className="font-serif text-lg font-bold sm:text-xl xl:text-2xl">
           Todoer
         </h3>
+
+        <Button className="gap-1 items-center">
+          <IconScriptPlus size={18} />
+          Add Task
+        </Button>
       </div>
     </div>
   );
